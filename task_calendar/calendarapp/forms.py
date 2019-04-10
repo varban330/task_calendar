fc95c50f921f4c5e2e975c292e4e9587fb757d8a
Varun Bansal <varun330@gmail.com>
2019-04-10 18:54:16 +0530

    Get method for dateform

--- task_calendar/calendarapp/forms.py
+++ task_calendar/calendarapp/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from datetime import datetime
+
+now = datetime.now()
 
 class DateForm(forms.Form):
     Options = (('1', 'January'),('2', 'February'),('3', 'March'),('4', 'April'),('5', 'May'),('6', 'June'),('7', 'July'),('8', 'August'),('9', 'September'),('10', 'October'),('11', 'November'),('12', 'December'))
     month = forms.ChoiceField(label='Month', widget=forms.Select, choices=Options)
-    year = forms.IntegerField(max_value = 9999, min_value = 1901)
+    year = forms.IntegerField(max_value = 9999, min_value = 1901, initial = int(now.strftime("%Y")))
 
     class Meta:
         fields = ["month","year"]
--- task_calendar/calendarapp/views.py
+++ task_calendar/calendarapp/views.py
@@ -15,8 +15,13 @@ class IndexView(View):
     template_name = "calendarapp/index.html"
 
     def get(self,request):
-        form = self.form_class(None)
-        currdate = datetime.now()
+        form = self.form_class(request.GET)
+        if not request.GET.keys():
+            currdate = datetime.now()
+        else:
+            month = request.GET["month"]
+            year = request.GET["year"]
+            currdate = datetime(int(year),int(month),10)
         currdate = isttime(currdate)
         month = currdate.month
         year = currdate.year
@@ -33,10 +38,38 @@ class IndexView(View):
         return render(request, self.template_name,{'form':form, 'blocks':blocks, 'currdate':currdate, "date_array":date_array, "sat_date_array":sat_date_array, "block_date_array":block_date_array ,"first_day":first_day, "first_date":first_date.date()})
 
     def post(self,request):
-        form = self.form_class(request.POST)
-        month = request.POST["month"]
-        year = request.POST["year"]
-        currdate = datetime(int(year),int(month),10)
+        form = self.form_class(request.GET)
+        print(request.POST)
+        date_req = request.POST['date']
+        print(date_req)
+        date_time = datetime.strptime(date_req,"%d/%m/%Y")
+        date = date_time.date()
+        status = request.POST['status']
+        task1 = request.POST['task1']
+        task2 = request.POST['task2']
+        task3 = request.POST['task3']
+        blocks  = DateInstance.objects.filter(date = date)
+        if blocks:
+            block = blocks[0]
+            block.status = status
+            block.task1 = task1
+            block.task2 = task2
+            block.task3 = task3
+            block.save()
+        else:
+            block = DateInstance()
+            block.status = status
+            block.task1 = task1
+            block.task2 = task2
+            block.task3 = task3
+            block.date = date
+            block.save()
+        if not request.GET.keys():
+            currdate = datetime.now()
+        else:
+            month = request.GET["month"]
+            year = request.GET["year"]
+            currdate = datetime(int(year),int(month),10)
         currdate = isttime(currdate)
         month = currdate.month
         year = currdate.year
